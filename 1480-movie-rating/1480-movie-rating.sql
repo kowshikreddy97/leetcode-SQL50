b952ce89850e5a6cfeb7894f3d7b90272f061c39
Kowshik Reddy <67260233+kowshikreddy97@users.noreply.github.com>
2025-01-17 17:17:15 -0600

    Time: 2739 ms (12.64%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,29 +1,28 @@
 # Write your MySQL query statement below
-
-WITH user_h_rating AS (
-    SELECT u.name, COUNT(m.rating) AS ratings_count
-    FROM MovieRating m
-    INNER JOIN Users u
-    USING(user_id)
-    GROUP BY m.user_id, u.name
-    ORDER BY ratings_count DESC, u.name ASC
+WITH more_reviews AS (
+    SELECT name, COUNT(movie_id) as movies_count
+    FROM Users 
+    JOIN MovieRating
+    USING (user_id)
+    GROUP BY user_id
+    ORDER BY movies_count DESC, name ASC
     LIMIT 1
 ),
 
 highest_average AS (
-    SELECT mv.title, AVG(m.rating) AS avg_rating
-    FROM MovieRating m
-    INNER JOIN Movies mv
-    USING(movie_id)
-    WHERE EXTRACT(month from created_at) = 2
-            AND EXTRACT(year from created_at) = 2020
-    GROUP BY m.movie_id
-    ORDER BY avg_rating DESC, mv.title ASC
+    SELECT movie_id, title, AVG(rating) AS avg_rating
+    FROM MovieRating
+    JOIN Movies
+    USING (movie_id)
+    WHERE EXTRACT(month FROM created_at) = 2
+            AND EXTRACT(year FROM created_at) = 2020
+    GROUP BY movie_id
+    ORDER BY avg_rating DESC, title ASC
     LIMIT 1
 )
 
 SELECT name AS results
-FROM user_h_rating
+FROM more_reviews
 UNION ALL
 SELECT title
 FROM highest_average;
